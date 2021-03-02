101d504f442c4c6fdb03f6e1ef0bc7e66f4cf31b
ourongxing <orongxing@gmail.com>
2021-03-02 22:02:27 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# CQUPT-ICS-Courses
-将课程表转换成 ICS 文件
+# ics4cqupt
+
+Generate .ics file for CQUPT.
+
+### Usage
+
+```shell
+yarn global add ics4cqupt
+# or
+npm install -i ics4cqupt
+# then
+ics4cqupt -i 2018210000
+# more
+ics4cqupt -h
+```
+
+### Thank
+
+- [ics](https://github.com/adamgibbons/ics)
+- [cqupt-ics](https://github.com/IsLand-x/cqupt-ics)
+
+### License
+MIT © ourongxing
--- index.js
+++ index.js
@@ -1,125 +1,114 @@
-let request = require("request");
-let cheerio = require("cheerio");
-let fs = require("fs");
-let chalk = require("chalk");
-let ics = require("ics");
+const fs = require("fs");
+const ics = require("ics");
+const yargs = require("yargs");
+const fetch = require("node-fetch");
 
-let newSemester = "Mon Mar 01 2021";
-let stuID = "2018210052";
+
+// 命令行参数
+const argv = yargs
+  .option('id', {
+    describe: '你的学号',
+    alias: 'i'
+  })
+  .option('day', {
+    default: '2021-03-01',
+    describe: '开学第一天',
+    alias: 'd'
+  })
+  .option('notice', {
+    default: 0,
+    describe: '提前通知时间（分钟），0 为不提醒',
+    alias: 'n'
+  })
+  .demand(['i'])
+  .help('h')
+  .usage('Usage: $0 [options]')
+  .alias('h', 'help')
+  .argv
+
+argv.notice = {
+  all: argv.notice,
+  m: argv.notice % 60,
+  h: parseInt(argv.notice / 60),
+}
 
 // 学期开始时间
-let semesterBegin = new Date(`${newSemester} 00:00:00`);
+const semesterBegin = new Date(`${argv.day} 00:00:00`);
 // 初始化第一周时间
-let weekday = new Array(7).fill(semesterBegin.getTime()).map((el, index) => {
+const weekday = new Array(7).fill(semesterBegin.getTime()).map((el, index) => {
   return el + index * 86400000;
 });
 // 设置课程开始时间
-let begin = [28800000, 36900000, 50400000, 58500000, 68400000, 74700000];
+const begin = [28800000, 36900000, 50400000, 58500000, 68400000, 74700000];
 // 设置一周时长
-let week = 86400000 * 7;
+const week = 86400000 * 7;
 // 课程数组
-let classTable = [];
-// 生成课表函数
-function generate(stuid) {
-  request(`http://jwzx.cqupt.edu.cn/kebiao/kb_stu.php?xh=${stuid}`, function (
-    err,
-    response,
-    body
-  ) {
-    if (err === null && response.statusCode === 200) {
-      console.log(chalk.blue("[Message]") + " 连接成功");
+const classTable = [];
 
-      let $ = cheerio.load(body);
-      let tb = $("#stuPanel tbody tr");
-      // 按行处理课表
-      for (let i = 0; i < tb.length; i++) {
-        let duration = cheerio.load(tb[i])("td").first().text();
-        let index = [
-          "1、2节",
-          "3、4节",
-          "5、6节",
-          "7、8节",
-          "9、10节",
-          "11、12节",
-        ].indexOf(duration);
-        if (["中午间歇", "下午间歇"].indexOf(duration) !== -1) {
-          console.log(chalk.cyan("[Scan Rows]") + " 跳过 ->" + duration + "<-");
-        } else {
-          console.log(
-            chalk.cyan("[Scan Rows]") + " 正在处理 ->" + duration + "<-"
-          );
-          addCourse(tb[i], index);
+// 生成课表
+generate();
+
+// 生成课表函数
+function generate() {
+  fetch("https://cyxbsmobile.redrock.team/api/kebiao", {
+    method: "post",
+    body: "stu_num=" + argv.id,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  }).then(res => res.json())
+    .then(res => {
+      if (res.success) {
+        for (let i of res.data) {
+          addCourse(i);
         }
+        // 处理成ics文件
+        const { error, value } = ics.createEvents(classTable);
+        if (error) {
+          console.log("生成 ics 文件失败");
+          return;
+        }
+        console.log("生成 ics 文件成功");
+        fs.writeFileSync(`${__dirname}/${argv.id}.ics`, value);
       }
-      // 处理成ics文件
-      console.log(chalk.blue("[Message]") + " 正在处理为日历ics文件");
-      const { error, value } = ics.createEvents(classTable);
-      if (error) {
-        console.log(chalk.red("[Error]" + error));
+      else {
+        console.log("获取课表失败");
         return;
       }
-      console.log(chalk.blue("[Message]") + " 成功转化为ics格式");
-      console.log(chalk.blue("[Message]") + " 正在生成ics文件");
-      fs.writeFileSync(`${__dirname}/${stuid}.ics`, value);
-      console.log(chalk.greenBright("[Success]") + " 生成ics文件成功!");
-      console.log(chalk.greenBright("[Message]" + " Created By IsLand 2020 Sept. 1st"))
-    }
-  });
+    })
 }
 
-function addCourse(tr, duration) {
-  let row = cheerio.load(tr);
-  let weekAndCourse = row("td:not(td[style])");
-  // 处理每个单元格
-  for (let i = 0; i < weekAndCourse.length; i++) {
-    console.log(
-      chalk.yellow("[Scan Weekday]") + " 正在处理周" + (i + 1) + "课程"
-    );
-    let courses = cheerio.load(weekAndCourse[i])("div");
-    // 处理每个单元格的课程
-    for (let j = 0; j < courses.length; j++) {
-      const course = cheerio.load(courses[j]);
-      let courseInfo = course.text();
-      let zc = course("div").attr("zc");
-      if (!courseInfo) {
-        continue;
-      } else {
-        let reg = /(.+)-(.+)地点：(.+)\s+(.+周)(.+)\s+(.+学分)/;
-        let array = reg.exec(courseInfo);
-        let lasttime = { hours: 1, minutes: 40 };
-        if (courseInfo.indexOf("3节连上") !== -1) {
-          lasttime = { hours: 3 };
-        } else if (courseInfo.indexOf("4节连上") !== -1) {
-          lasttime = { hours: 3, minutes: 55 };
-        }
-        // 处理课程每周
-        for (let k = 0; k < zc.length; k++) {
-          if (zc.charAt(k) == 1) {
-            classTable.push({
-              title: array[2],
-              location: array[3],
-              description:
-                array[4] + " " + array[5] + " " + array[6] + " " + array[1],
-              start: formatDate(weekday[i] + week * k + begin[duration]),
-              duration: lasttime,
-              status: "TENTATIVE",
-              categories: ["重邮课程", array[2]],
-              alarms: [
-                {
-                  action: "display",
-                  trigger: { hours: 0, minutes: 30, before: true },
-                },
-              ],
-              busyStatus: "BUSY",
-              calName: "重邮课表",
-            });
-          }
-        }
-        console.log(
-          chalk.magenta("[Add Success]") + " 处理完成课程 -> " + array[2]
-        );
-      }
+function addCourse(courseInfo) {
+  let duration = { hours: 1, minutes: 40 };
+  if (courseInfo.rawWeek.indexOf("3节连上") !== -1) {
+    duration = { hours: 3 };
+  } else if (courseInfo.rawWeek.indexOf("4节连上") !== -1) {
+    duration = { hours: 3, minutes: 55 };
+  }
+
+  // 处理课程每周
+  for (let index of courseInfo.week) {
+    let info = {
+      title: courseInfo.course,
+      location: courseInfo.classroom,
+      description: `${courseInfo.teacher} ${courseInfo.type} ${courseInfo.rawWeek}`,
+      start: formatDate(weekday[courseInfo.hash_day] + week * (index - 1) + begin[(courseInfo.begin_lesson - 1) / 2]),
+      duration: duration,
+      status: "TENTATIVE",
+      categories: ["课程表", courseInfo.course],
+      busyStatus: "BUSY",
+      calName: "课程表",
     }
+    // 提前通知，通过文件导入生效，通过链接导入不生效，默认不通知
+    if (argv.notice.all) {
+      info.alarms = [
+        {
+          action: "display",
+          trigger: { hours: argv.notice.h, minutes: argv.notice.m, before: true },
+        }
+      ]
+    }
+    classTable.push(info);
   }
 }
 
@@ -133,5 +122,3 @@ function formatDate(time) {
     date.getMinutes(),
   ];
 }
-
-generate(stuID);
